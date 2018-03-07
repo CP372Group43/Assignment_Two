89b3bc8554755fed51274fe80e94ee97d18f06a8
Ben Douglas <dougers437@gmail.com>
2018-03-07 10:54:37 -0500

    Redoing the gui

--- Receiver.java
+++ Receiver.java
@@ -19,14 +19,15 @@ import java.net.*;
 @SuppressWarnings("serial")
 public class Receiver extends JFrame implements ActionListener {
 	
-	public static final int WIDTH = 500;
-    public static final int HEIGHT = 350;
+	public static final int WIDTH = 300;
+    public static final int HEIGHT = 455;
     
     public static JTextField type_text_field;
     public static JTextField host_text_field;
     public static JTextField ackport_text_field;
     public static JTextField port_text_field;
     public static JTextField body_text_area;
+    public static JTextField timeout_text_field;
     public static JTextArea response_text_area;
     public static JButton connect_button;
 
@@ -44,61 +45,70 @@ public class Receiver extends JFrame implements ActionListener {
     		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     		setResizable(false);
 
-    		// init all our panels
-    		JPanel server_panel = new JPanel();
-    		JPanel port_panel = new JPanel();
-        	JPanel message_panel = new JPanel();
-        	JPanel response_panel = new JPanel();
+    		// init our wrapper panel
         	JPanel wrapper_panel = new JPanel();
-        	JPanel actions_panel = new JPanel();
     		
         	// receiver IP field
+        	JPanel receiver_ip_panel = new JPanel();
+        	receiver_ip_panel.add(new JLabel("Receiver IP: "));
     		host_text_field = new JTextField("", 10);
     		host_text_field.setBackground(Color.WHITE);
+    		receiver_ip_panel.add(host_text_field);
     		
     		// receiver port field
+        	JPanel receiver_port_panel = new JPanel();
+        	receiver_port_panel.add(new JLabel("Receiver Port: "));
     		port_text_field = new JTextField("", 10);
     		port_text_field.setBackground(Color.WHITE);
+    		receiver_port_panel.add(port_text_field);
     		
     		// sender ack port field
+        	JPanel sender_ack_port = new JPanel();
+        	sender_ack_port.add(new JLabel("Sender Ack Port: "));
     		ackport_text_field = new JTextField("",10);
     		ackport_text_field.setBackground(Color.WHITE);
-    		
+    		sender_ack_port.add(ackport_text_field);
+
     		// file name
+        	JPanel file_panel = new JPanel();
+        	file_panel.add(new JLabel("File Name: "));
     		body_text_area = new JTextField("", 10);
     		body_text_area.setBackground(Color.WHITE);
+    		file_panel.add(body_text_area);
     		
+    		// timeout 
+        	JPanel timeout_panel = new JPanel();
+        	timeout_panel.add(new JLabel(" Timeout: "));
+    		timeout_text_field = new JTextField("",10);
+    		timeout_text_field.setBackground(Color.WHITE);
+    		timeout_panel.add(timeout_text_field);
+
     		// response
-    		response_text_area = new JTextArea(10, 20);
+        	JPanel response_panel = new JPanel();
+    		response_text_area = new JTextArea(10, 17);
     		response_text_area.setEditable(false);
     		response_text_area.setBackground(Color.WHITE);
     		response_text_area.setLineWrap(true);
     		response_text_area.setWrapStyleWord(true);
     		JScrollPane response_scroll_panel = new JScrollPane(response_text_area);
-    		
-    		// adding labels and inputs to panels
-    		server_panel.add(new JLabel("Sender IP: "));
-    		server_panel.add(host_text_field);
-    		port_panel.add(new JLabel("Receiver Port: "));
-    		port_panel.add(port_text_field);
-    		port_panel.add(new JLabel("Sender ACK Port: "));
-    		port_panel.add(ackport_text_field);
-    		message_panel.add(new JLabel("File Name: "));
-    		message_panel.add(body_text_area);
     		response_panel.add(new JLabel("Response: "));
     		response_panel.add(response_scroll_panel);
-    		wrapper_panel.add(server_panel, BorderLayout.NORTH);
-    		wrapper_panel.add(port_panel, BorderLayout.NORTH);
-    		wrapper_panel.add(message_panel, BorderLayout.CENTER);
     		
     		// add Send Button
+        	JPanel actions_panel = new JPanel();
         JButton send_button = new JButton("Send"); 
         send_button.setActionCommand("send");
         send_button.addActionListener(this);
-        actions_panel.add(send_button, BorderLayout.WEST); 
-        
-        wrapper_panel.add(response_panel, BorderLayout.CENTER);
-        wrapper_panel.add(actions_panel, BorderLayout.CENTER);
+        actions_panel.add(send_button); 
+    		
+    		// laying out the wrapper pannel
+    		wrapper_panel.add(receiver_ip_panel);
+    		wrapper_panel.add(receiver_port_panel);
+    		wrapper_panel.add(sender_ack_port);
+    		wrapper_panel.add(file_panel);
+    		wrapper_panel.add(timeout_panel);
+        wrapper_panel.add(actions_panel);
+        wrapper_panel.add(response_panel);
     		
     		add(wrapper_panel);
     		
--- Sender.java
+++ Sender.java
@@ -2,13 +2,12 @@ import java.io.* ;
 import java.net.* ;
 import java.util.* ;
 
-
-
 public class Sender implements Runnable{
 	private int hostport,hostackport,timeout;
 	private String datafile,hostaddress;
 	DatagramSocket serversocket = null;
 	DatagramSocket acksocket = null;
+	
 	public static void main(String[] args) {
 		int seqport,ackport,time;
 		String sendfile,hostaddr;
@@ -17,7 +16,9 @@ public class Sender implements Runnable{
 		ackport=Integer.parseInt(args[2]);
 		sendfile=args[3];
 		time=Integer.parseInt(args[2]);
-		new Sender(hostaddr,seqport,ackport,sendfile,time);	}
+		new Sender(hostaddr,seqport,ackport,sendfile,time);	
+	}
+	
 	public Sender(String hostaddr,int seqport,int ackport, String sendfile, int time) {
 		
 		this.hostaddress=hostaddr;
@@ -30,26 +31,26 @@ public class Sender implements Runnable{
 	}
 
 	public void run() {
-    	System.out.print("Sender On \n");
-    	try {
-    		this.serversocket = new DatagramSocket(this.hostport);
-    		this.acksocket= new DatagramSocket(this.hostackport,InetAddress.getByAddress(this.hostaddress.getBytes()));
-    		System.out.println(this.acksocket.getInetAddress());
-    		System.out.println(this.serversocket.getInetAddress());
-
-    	}catch(Exception e){
-    		e.printStackTrace(System.out);
-    	}
-    	while(true) {
+	    	System.out.print("Sender On \n");
+	    	try {
+	    		this.serversocket = new DatagramSocket(this.hostport);
+	    		this.acksocket= new DatagramSocket(this.hostackport,InetAddress.getByAddress(this.hostaddress.getBytes()));
+	    		System.out.println(this.acksocket.getInetAddress());
+	    		System.out.println(this.serversocket.getInetAddress());
+	
+	    	}catch(Exception e){
+	    		e.printStackTrace(System.out);
+	    	}
+	    	while(true) {
     		try {
     			new Thread(new UdpSender(this.serversocket,this.datafile,this.timeout,this.acksocket));
     		}catch(Exception e) {
-    			System.out.println(e.getMessage());
+			System.out.println(e.getMessage());
         		e.printStackTrace(System.out);
         		this.serversocket.close();
         		this.acksocket.close();
-    			System.exit(1);
-    		}
+	    		System.exit(1);
+	    	}
     	}
 
 	}
